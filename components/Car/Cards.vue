00bf370592e6b57e42630d38f406bafa4535f3ae
Hairul Ardhan Nawazaki <hanawazaki002@gmail.com>
2023-08-08 16:55:56 +0700

    add image optimize

--- components/Car/Cards.vue
+++ components/Car/Cards.vue
@@ -1,52 +1,6 @@
 <template>
   <div class="w-full">
-    <!-- card -->
     <CarCard v-for="car in cars" :key="car.id" :car="car" />
-    <!-- card -->
-    <!-- card -->
-    <!-- <div
-      class="shadow border w-full overflow-hidden mb-5 cursor-pointer h-[200px]"
-    >
-      <div class="flex h-full">
-        <img
-          src="https://images.unsplash.com/photo-1515592021493-9a2fe91005c4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80"
-          alt=""
-          class="w-[300px] h-full"
-        />
-        <div class="p-4 flex flex-col">
-          <div>
-            <h1 class="text-2xl text-blue-700">Sedan</h1>
-            <p class="text-gray-700">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            </p>
-          </div>
-          <h1 class="mt-auto text-xl">$190</h1>
-        </div>
-      </div>
-    </div> -->
-    <!-- card -->
-    <!-- card -->
-    <!-- <div
-      class="shadow border w-full overflow-hidden mb-5 cursor-pointer h-[200px]"
-    >
-      <div class="flex h-full">
-        <img
-          src="https://images.unsplash.com/photo-1489824904134-891ab64532f1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1031&q=80"
-          alt=""
-          class="w-[300px] h-full"
-        />
-        <div class="p-4 flex flex-col">
-          <div>
-            <h1 class="text-2xl text-blue-700">VW Beetle</h1>
-            <p class="text-gray-700">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            </p>
-          </div>
-          <h1 class="mt-auto text-xl">$190</h1>
-        </div>
-      </div>
-    </div> -->
-    <!-- card -->
   </div>
 </template>
 
